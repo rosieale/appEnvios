d0cc37aa95663e1a5575b8278f5b752ab24b93b1
Rosie-Maldonado <rosiefordummies@gmail.com>
2022-02-20 13:10:26 -0800

    lab 1 avance 2 -  clases agregadas en modelos

---
+++ models/seguimientos.js
@@ -0,0 +1,12 @@
+class seguimiento{
+    constructor(idEnvio,fecha,hora,lugar){
+        this.idEnvio = idEnvio
+        this.fecha = fecha
+        this.hora = hora
+        this.lugar = lugar
+    }
+}
+
+module.exports = {
+    seguimiento
+}
